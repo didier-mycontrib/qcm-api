b89fedc4c8c22f3b7a3ef6630213191df6a8e180
Didier_Defrance <didier@d-defrance.fr>
2025-08-19 12:16:42 +0200

    s

--- lancer_node_localMongo.bat
+++ lancer_node_localMongo.bat
@@ -1,5 +1,10 @@
 set PORT=8230
-set MONGODB_URL=mongodb://127.0.0.1:27017
+REM  MONGODB_URL=mongodb://127.0.0.1:27017
+set MONGODB_HOSTNAME=127.0.0.1
+set MONGODB_PORT=27017
+REM set MONGODB_USERNAME=root
+REM set MONGODB_PASSWORD=root
+REM set MONGODB_PASSWORD_FILE=my_temp_secret_pwd.txt
 REM node server.js
 nodemon server.js
 pause
--- qcm-db-mongoose.js
+++ qcm-db-mongoose.js
@@ -1,8 +1,29 @@
 import mongoose from 'mongoose';
+import fs from 'fs/promises';
 
-function thisDbFn(){
+async function readBasicTextFile(fileName){
+	let data = await fs.readFile(fileName);
+	return data;
+}
 
+async function thisDbFn(){
    let mongoDbUrl = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017"; //by default
+   let mongoDbHostname = process.env.MONGODB_HOSTNAME || "127.0.0.1"; //by default
+   let mongoDbPort = process.env.MONGODB_PORT || "27017"; //by default
+   let mongoDbUsername = process.env.MONGODB_USERNAME ;
+   let mongoDbPassword = process.env.MONGODB_PASSWORD ; 
+   let mongoDbPasswordFile = process.env.MONGODB_PASSWORD_FILE ; //if password in secret file
+   if(process.env.MONGODB_HOSTNAME){
+	   //MONGODB_HOSTNAME est une variable d'env plus prioritaire que 
+	   mongoDbUrl = `mongodb://${mongoDbHostname}:${mongoDbPort}` 
+   }
+   if(process.env.MONGODB_PASSWORD_FILE){
+	   //MONGODB_PASSWORD_FILE est une variable d'env plus prioritaire que MONGODB_PASSWORD
+	   console.log("password is stored in basic secret file:= " + mongoDbPasswordFile);
+	   mongoDbPassword = await readBasicTextFile(mongoDbPasswordFile);
+	   console.log("mongoDbPassword (read from file)= " + mongoDbPassword);
+   }
+   console.log("mongoDbUrl="+mongoDbUrl);
    let cnxOptions = {}
    if(process.env.TEST_MODE=="IT"){
       cnxOptions = { directConnection: true , dbName : 'qcm_db' }
@@ -10,8 +31,13 @@ function thisDbFn(){
     else{
       cnxOptions = { dbName : 'qcm_db'}
     }
+	
+	if(mongoDbUsername && mongoDbPassword){
+		cnxOptions.auth = { username :mongoDbUsername , password: mongoDbPassword};
+		console.log("cnxOptions.auth= " + JSON.stringify(cnxOptions.auth));
+	}
 
-   console.log("mongoDbUrl="+mongoDbUrl);
+   
    mongoose.connect(mongoDbUrl, cnxOptions);
    var thisDb  = mongoose.connection;
 
